23989cd83e2431e7a86d409e7d376cc90f770f33
sgill33 <sgill33@calpoly.edu>
2024-01-11 20:47:59 -0800

    added form to insert characters into table

--- packages/react-frontend/src/myApp.js
+++ packages/react-frontend/src/myApp.js
@@ -1,43 +1,10 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
-const characters = [
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ];
 
 function MyApp() {
     const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor"
-          },
-          {
-            name: "Mac",
-            job: "Bouncer"
-          },
-          {
-            name: "Dee",
-            job: "Aspring actress"
-          },
-          {
-            name: "Dennis",
-            job: "Bartender"
-          }
     ]);
 
     function removeOneCharacter(index) {
@@ -49,10 +16,12 @@ function MyApp() {
 
     return (
         <div className="container">
-        <Table 
-            characterData = {characters}
-            removeCharacter={removeOneCharacter}/>
-    </div>
-  );
+          <Table
+            characterData={characters}
+            removeCharacter={removeOneCharacter}
+          />
+          <Form />
+        </div>
+    );
 }
 export default MyApp;
